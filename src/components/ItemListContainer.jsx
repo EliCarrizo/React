dd85a09de483a0a1f8904aa831d199c8d2e7c566
Eliana Carrizo <90808506+EliCarrizo@users.noreply.github.com>
2023-10-06 20:12:34 -0300

    Change the name of the listener item

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -7,19 +7,19 @@ const ItemListContainer = () => {
     console.log(categoria)
 
     const productos = [
-        {id: 1, nombre:"GOKU", descripcion: "Descripcion del producto A", stock: 10, categoria: "A"},
-        {id: 2, nombre:"VEGETTA", descripcion: "Descripcion del producto B", stock: 15,categoria: "A"},
-        {id: 3, nombre:"CELL", descripcion: "Descripcion del producto C", stock: 4, categoria: "B"},
-        {id: 4, nombre:"BULMA", descripcion: "Descripcion del producto D", stock: 60, categoria: "B"},
-        {id: 5, nombre:"SEIYA", descripcion: "Descripcion del producto E", stock: 100, categoria: "C"},
-        {id: 6, nombre:"ANDROMEDA", descripcion: "Descripcion del producto F", stock: 2, categoria: "C"},
+        {id: 1, nombre:"GOKU", descripcion: "Descripcion GOKU", stock: 10, categoria: "A"},
+        {id: 2, nombre:"VEGETTA", descripcion: "Descripcion VEGETTA", stock: 15,categoria: "A"},
+        {id: 3, nombre:"CELL", descripcion: "Descripcion CELL", stock: 4, categoria: "B"},
+        {id: 4, nombre:"BULMA", descripcion: "Descripcion BULMA", stock: 60, categoria: "B"},
+        {id: 5, nombre:"SEIYA", descripcion: "Descripcion SEIYA", stock: 100, categoria: "C"},
+        {id: 6, nombre:"ANDROMEDA", descripcion: "Descripcion ANDROMEDA", stock: 2, categoria: "C"},
     ]
 
     const getProductos = new Promise((resolve, reject) => {
       if(productos.length > 0 ) {
           setTimeout (() => {
               resolve(productos)
-          }, 1500)
+          }, 5000)
       } else {
           reject(new Error("No hay datos registrados"))
       }
